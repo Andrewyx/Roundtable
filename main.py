d7490564662a00fad4a37f0d9c5f05eac3e4823f
Andrew Mao <andrewyxmao@gmail.com>
2023-10-22 23:40:11 -0700

    added pickles...now has cache!

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from datetime import date
+import pickle
 
 def get_data(url) -> list:
     browser_options = ChromeOptions()
-    browser_options.headless = True
+    browser_options.add_argument("--headless=new")
     
     driver = Chrome(options=browser_options)
     driver.get(url)
@@ -16,27 +17,35 @@ def get_data(url) -> list:
     listofdays = driver.find_elements(By.CSS_SELECTOR, ".menu-day-contents.font-normal.height11")
     
 
-    data = []
-
+    data = {}
     for count, day in enumerate(listofdays):
         dishes = day.find_elements(By.CSS_SELECTOR, ".food-name")
         menu = []
         for dish in dishes:
+            
             food_item = dish.get_attribute("textContent").replace("\\n", "").strip()
             menu.append(food_item)
         if len(menu) > 0:
-            day_menu = {
-                f"Day {count + 1}" : menu
-            }
-            data.append(day_menu)
+            data[f'Day {count + 1}'] = menu
 
     driver.quit()
     return data
 
 
 def main():
-    data = get_data(f"https://ubc.nutrislice.com/menu/ubc-gather-place-vanier-residence/gather-place-vanier-residence-lunch/print-menu/month/{date.today()}")
-    print(data[date.today().day])
+    try:
+        file = open("menus.txt", "rb")
+        cache = pickle.load(file)
+        print(cache[f"Day {date.today().day}"])
+        file.close()
+    except Exception as error:
+        print(error)
+        data = get_data(f"https://ubc.nutrislice.com/menu/ubc-gather-place-vanier-residence/gather-place-vanier-residence-lunch/print-menu/month/{date.today()}")
+        print(data[f"Day {date.today().day}"])
+        file = open("menus.txt", "wb")
+        pickle.dump(data, file)
+
+        file.close()
 
 
 if __name__ == '__main__':
