ee342b8bdad1ffbfc95d9dfd7aa5e29209f0d6e3
Andrew Mao <andrewyxmao@gmail.com>
2023-10-22 22:33:11 -0700

    working menus

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from selenium.webdriver import Chrome, ChromeOptions
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import time
 
 def get_data(url) -> list:
     browser_options = ChromeOptions()
@@ -14,22 +15,20 @@ def get_data(url) -> list:
     wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "menu-day-contents.font-normal.height11")))
     listofdays = driver.find_elements(By.CSS_SELECTOR, ".menu-day-contents.font-normal.height11")
     
-    #ListOfPage -> ListOfDay
-    #produces 7 days for each page    
 
+    data = []
 
-    data = []  
-
-    for day in listofdays:
+    for count, day in enumerate(listofdays):
         dishes = day.find_elements(By.CSS_SELECTOR, ".food-name")
-        for count, dish in enumerate(dishes):
-            food_item = {
-                f"dish{count}" : dish.get_attribute("textContent").replace("\\n", "").strip()
+        menu = []
+        for dish in dishes:
+            food_item = dish.get_attribute("textContent").replace("\\n", "").strip()
+            menu.append(food_item)
+        if len(menu) > 0:
+            day_menu = {
+                f"Day {count}" : menu
             }
-            data.append(food_item)
-
-    
-
+            data.append(day_menu)
 
     driver.quit()
     return data
@@ -37,7 +36,8 @@ def get_data(url) -> list:
 
 def main():
     data = get_data("https://ubc.nutrislice.com/menu/ubc-gather-place-vanier-residence/gather-place-vanier-residence-lunch/print-menu/month/2023-10-22")
-    print(f"here is my -> {data}")
+    for dish in data:
+        print(f"{dish}")
 
 
 if __name__ == '__main__':
