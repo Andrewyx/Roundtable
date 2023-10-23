c7787e1b1e07a0ed98e99fa85240e428637e78a3
Andrew Mao <andrewyxmao@gmail.com>
2023-10-22 22:15:41 -0700

    working jsons

--- main.py
+++ main.py
@@ -11,27 +11,32 @@ def get_data(url) -> list:
     driver.get(url)
 
     wait = WebDriverWait(driver, 10)
-    wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "menu-week-container")))
-    books = driver.find_elements(By.CSS_SELECTOR, ".menu-week-container")
-    print(books)
-    data = [books]
-    # for book in books:
-    #     title = book.find_element(By.CSS_SELECTOR, "h3 > a")
-    #     price = book.find_element(By.CSS_SELECTOR, ".price_color")
-    #     stock = book.find_element(By.CSS_SELECTOR, ".instock.availability")
-    #     book_item = {
-    #         'title': title.get_attribute("title"),
-    #         'price': price.text,
-    #         'stock': stock. text
-    #     }
-    #     data.append(book_item)
+    wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "menu-day-contents.font-normal.height11")))
+    listofdays = driver.find_elements(By.CSS_SELECTOR, ".menu-day-contents.font-normal.height11")
+    
+    #ListOfPage -> ListOfDay
+    #produces 7 days for each page    
+
+
+    data = []  
+
+    for day in listofdays:
+        dishes = day.find_elements(By.CSS_SELECTOR, ".food-name")
+        for count, dish in enumerate(dishes):
+            food_item = {
+                f"dish{count}" : dish.get_attribute("textContent").replace("\\n", "").strip()
+            }
+            data.append(food_item)
+
+    
+
 
     driver.quit()
     return data
 
 
 def main():
-    data = get_data("https://ubc.nutrislice.com/menu/ubc-gather-place-vanier-residence/gather-place-vanier-residence-breakfast/print-menu/month/2023-10-22")
+    data = get_data("https://ubc.nutrislice.com/menu/ubc-gather-place-vanier-residence/gather-place-vanier-residence-lunch/print-menu/month/2023-10-22")
     print(f"here is my -> {data}")
 
 
